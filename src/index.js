5fdc0d4a4a4b8abd79098807e7b181bbb4255544
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-02-10 15:58:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 
 module.exports = function towelSort (matrix) {
   let Matrix = [];
+  if (matrix.length === 0){return[]};
+  if (matrix == undefined){return[]};
   for (i = 0; i < matrix.length; i++){
     for(j = 0; j < matrix[i].length; j++){
         if (i % 2 == 0){
